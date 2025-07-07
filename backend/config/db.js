3413f7e6f7af38fff55e4d601d8e7f22183d8eff
Supersimplelang <captinangelo2004@gmail.com>
2025-07-07 23:54:56 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  console.log("Connecting to:", process.env.MONGO_URI); 
   try {
       await mongoose.connect(process.env.MONGO_URI);
       console.log("Successfully connected to mongoDB👍")
