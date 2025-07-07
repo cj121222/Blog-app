b09817d7d29bc9cd7e4fa999d278064604986a60
Supersimplelang <captinangelo2004@gmail.com>
2025-07-08 00:35:39 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api/": "https://blog-app-backend-6a8t.onrender.com",
     },
   };
 });
